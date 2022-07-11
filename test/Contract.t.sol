3eb0fb82352cdf0e4f9a6fcf448ba5c2c4c83c5f
hc <herbert.cuyugan@outlook.com>
2022-07-12 04:09:19 +1000

    first commit

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,26 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "src/Counter.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
-
-    function testExample() public {
-        assertTrue(true);
+    Counter counter;
+    function setUp() public {
+        counter = new Counter(10);
     }
+
+    function testGetCount() public {
+        int value = counter.getCount();
+        assertEq(value, 10);
+        // you need to explicitely express how verbose you want it to be say forge test -vvvv
+        emit log_named_int("The value is: ", value);
+    }
+
+    function testIncrementCounter() public {
+        counter.incrementCounter();
+        int value = counter.getCount();
+        assertEq(value, 11);
+        emit log_named_int("The value is: ", value);
+    }
+
 }
